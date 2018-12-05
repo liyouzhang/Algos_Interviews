c556cdfb58f7004de64dd368b91d5c7ff19360f1
Liyou Zhang <liyou713@gmail.com>
2018-12-04 18:55:49 -0800

    work in progress 579

--- sum_of_three_values.py
+++ sum_of_three_values.py
@@ -13,3 +13,19 @@ def sum_three_one(arr,val):
                     return True
     return False
 
+    #Space - O(1)
+    #Run time - O(n^2)
+
+def sum_three_two(arr,val):
+    '''
+    approach 2:
+    1. sort the array
+    2. have 2 idx
+    3. check if val - idx1 - idx2 still in the array between these 2 idx
+    4. if yes, then 
+
+    '''
+    arr.sort()
+    i = 0
+    j = len(arr)-1
+    for i in 
